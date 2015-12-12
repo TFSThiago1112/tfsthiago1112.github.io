61bc560259728b6dd575908bfdcbd5be9d965221
Thiago França da Silva <tfsthiago1112@hotmail.com>
2015-12-12 16:26:44 -0200

    Update Bootstrap and Font Awesome Files Fix Logo and Fonts Urls and Fix Google Access

--- assets/js/eScript/eScript.DateTime.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    var interval = setInterval(function() {
-        var momentNow = new Date();
-        $('.clock').html(momentNow.toLocaleDateString() + ' ' + momentNow.toLocaleTimeString());
-    }, 10);
-    var momentGen = new Date();
-    $('.gentime').html(momentGen.toLocaleDateString() + ' ' + momentGen.toLocaleTimeString() + ':' + momentGen.getMilliseconds());
-});
